feeed414b0d576319276e16a71e96358daf678a2
realmattclark <54007279+realmattclark@users.noreply.github.com>
2022-03-06 17:25:13 -0600

    Update source

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,7 +1,42 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<iomanip>
+
 using namespace std;
+void printCustomersData(const int id[], const string first[], const string last[], const double savings[], const double checkings[], int n) {
+
+    cout << setw(10) << left << "ID" << " ";
+    cout << setw(15) << left << "First" << " ";
+    cout << setw(15) << left << "Last" << " ";
+    cout << setw(20) << left << "Saving Account" << " ";
+    cout << setw(20) << left << "Checking Account" << " " << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << setw(10) << left << id[i] << " ";
+        cout << setw(15) << left << first[i] << " ";
+        cout << setw(15) << left << last[i] << " ";
+        cout << setw(20) << left << savings[i] << " ";
+        cout << setw(20) << left << checkings[i] << " " << endl;
+    }
+    cout << "\n\n";
+}
+void printTotal(const int id[], const double savings[], const double checkings[], int n) {
+    cout << setw(10) << left << "ID" << " ";
+    cout << setw(15) << left << "Saving Account" << " ";
+    cout << setw(20) << left << "Checking Account" << " ";
+    cout << setw(20) << left << "Total" << " " << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << setw(10) << left << id[i] << " ";
+        cout << setw(15) << left << savings[i] << " ";
+        cout << setw(20) << left << checkings[i] << " ";
+        cout << setw(20) << left << checkings[i] + savings[i] << " " << endl;
+    }
+    cout << "\n\n";
+}
+
 
 int main() {
-	cout << "Hello World" << endl;
+	
 }
